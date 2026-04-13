e1a0b2fa84f45d7a7050b8e0c6ec9e909e237ede
murkenmedia <brian@murkenmedia.com>
2026-04-13 13:14:47 -0700

    Post single with custom tax filters

--- wp-content/themes/sig/template-parts/content.php
+++ wp-content/themes/sig/template-parts/content.php
@@ -26,7 +26,7 @@ $insightstitle = get_blog_title();
 $breadcrumb = '<p class="hero__content__pretitle mb-4 blue-medium-link"><a href="'.$insightslink.'">'.$insightstitle.'</a></p>';
 //$hero = get_post_hero($id,$insightslink,$insightstitle);
 
-$date = '<p class="has-blue-color mb-1">'.get_the_date('F j, Y', $id).'</p>';
+//$date = '<p class="has-green-color mb-1">'.get_the_date('F j, Y', $id).'</p>';
 /* $hero = '
 <div class="hero alignfull secondary-hero insights-hero ">
     <div class="hero__content max-xl text-center">
@@ -46,8 +46,10 @@ $img = '';
 if(get_field('disable_featured_img')) {
     $headerclass = ' no-featured-img';
 } else {
+    
     if(has_post_thumbnail($id)) {
         $imgid = get_post_thumbnail_id($id);
+        $headerclass = ' has-featured-img';
         $img = '
         <div class="insights-single__featured-img">
             '.wp_get_attachment_image($imgid, 'full').'
@@ -63,7 +65,7 @@ if(get_field('disable_featured_img')) {
 ///CATS & TAGS
 $topiclinks = '';
 
-$tagsarr = array();
+/* $tagsarr = array();
 $post_tags = get_the_tags($id);
 if ( $post_tags ) {
 	foreach( $post_tags as $tag ) {
@@ -88,20 +90,62 @@ if ( $post_cats ) {
         </li>
         ';
     }
+} */
+
+$themelinks = $terms = '';
+$themearr = array();
+$terms = get_the_terms( $id, 'insight_theme' );
+if ( $terms ) {
+	foreach( $terms as $term ) {
+        array_push($themearr, $term->term_id);
+        
+        $themelinks .= '
+        <li>
+            <a href="'.$insightslink.'#'.$term->slug.'">'.$term->name.'</a>
+        </li>
+        ';
+    }
+}
+$solutionlinks = '';
+$solutionsarr = array();
+$terms = get_the_terms( $id, 'insight_solution' );
+if ( $terms ) {
+	foreach( $terms as $term ) {
+        array_push($solutionsarr, $term->term_id);
+        
+        $solutionlinks .= '
+        <li>
+            <a href="'.$insightslink.'#'.$term->slug.'">'.$term->name.'</a>
+        </li>
+        ';
+    }
 }
 
 $authors = '';
 
 
 /////SIDEBAR
-$topics = '';
-if($topiclinks !== '') {
-    $topics = '
+$themes = '';
+if($themelinks !== '') {
+    $themes = '
     <div class="insights-single__sidebar__module insights-sidebar-topics mb-5">
 
-        <h4 class="mb-3 sans-bold has-small-font-size has-blue-color">'.__('Topics Covered', 'sig').'</h4>
+        <h4 class="mb-3 sans-bold has-small-font-size has-blue-color">'.__('Themes', 'sig').'</h4>
         <ul class="insights-single__sidebar__links list-unstyled has-small-font-size">
-            '.$topiclinks.'
+            '.$themelinks.'
+        </ul>
+
+    </div>';
+}
+
+$solutions = '';
+if($themelinks !== '') {
+    $solutions = '
+    <div class="insights-single__sidebar__module insights-sidebar-topics mb-5">
+
+        <h4 class="mb-3 sans-bold has-small-font-size has-blue-color">'.__('Solutions', 'sig').'</h4>
+        <ul class="insights-single__sidebar__links list-unstyled has-small-font-size">
+            '.$solutionlinks.'
         </ul>
 
     </div>';
@@ -110,22 +154,21 @@ if($topiclinks !== '') {
 $relatedlinks = '
 <div class="insights-single__sidebar__module insights-sidebar-related">
 
-    <h4 class="mb-3 sans-bold has-small-font-size has-blue-color">'.__('Related Articles', 'sig').'</h4>
+    <h4 class="mb-3 sans-bold has-small-font-size has-blue-color">'.__('Related Insights', 'sig').'</h4>
 
     <ul class="insights-single__sidebar__links list-unstyled has-small-font-size">
-        '.get_related_blog($id,$tagsarr,$catsarr,'link',4).'
+        '.get_related_insights($id,$themearr,$solutionsarr,'link',4).'
     </ul>
 
 </div>
 ';
 
+//<p class="insights-single__header__date mb-0">'.get_the_date('F j, Y', $id).'</p> 
+
 $header = '
-<header class="insights-single__header has-text-align-center'.$headerclass.'">
-    <p class="sans-bold in-content-breadcrumb-link"><a href="'.$insightslink.'">'.$insightstitle.'</a></p>
-    <h1 class="fade-in has-blue-dark-color mb-4 '.$titleclass.'">'.$title.'</h1>
-    <div class="entry-meta">
-        '.$date.'
-    </div>
+<header class="insights-single__header '.$headerclass.'">
+    '.$breadcrumb.'
+    <h1 class="insights-single__header__title mb-0 '.$titleclass.'">'.$title.'</h1>
 </header>';
 
 ?>
@@ -148,7 +191,7 @@ $header = '
 
         <div class="insights-single__sidebar">
             <div class="insights-single__sidebar__sticky mt-6 mt-md-0">
-                <?php echo $topics; ?>
+                <?php echo $themes.$solutions; ?>
                 <?php echo $relatedlinks; ?>
             </div>
         </div>
@@ -159,7 +202,7 @@ $header = '
 
         <div class="insights-single__footer__cat text-center text-md-right">
             <ul class="insights-single__footer__cat-list sans-500">
-                <?php echo $topiclinks; ?>
+                <?php //echo $themelinks; ?>
             </ul>
         </div>
 
@@ -176,7 +219,7 @@ $header = '
             <h4 class="text-center mb-6 has-large-font-size sans-500 has-blue-dark-color">Related Insights</h4>
 
             <div class="max-xl mx-auto tiles-grid tiles-three-col tiles-stacked-content">
-                <?php echo get_related_blog($id,$tagsarr,$catsarr,'',3); ?>
+                <?php echo get_related_insights($id,$tagsarr,$catsarr,'',3); ?>
             </div>
 
         </div>        
